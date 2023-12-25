9e9f46dedec4978c5e3038fca43a774fb0119196
adas0mahdi <adas0mahdi@gmail.com>
2023-12-25 17:54:58 +0800

    Links

--- app.vue
+++ app.vue
@@ -13,8 +13,8 @@ function view(name) {
   margin-bottom: 30px;
   align-items: center;
   ">
-    <a href="#" @click="view('home')">Home</a>
-    <a href="#" @click="view('counter')">Counter</a>
+    <a style=" color: rgb(5, 142, 96); text-decoration:none;" href="#" @click="view('home')">Home</a>
+    <a style=" color: rgb(5, 142, 96); text-decoration:none;" href="#" @click="view('counter')">Counter</a>
   </div>
   <div style="display: flex;
   justify-content: center;
